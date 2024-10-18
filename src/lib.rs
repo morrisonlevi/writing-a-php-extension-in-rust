b21ef9b1cf8515c591228452a6875493f777decb
Levi Morrison <levi.morrison@datadoghq.com>
2024-10-18 14:39:33 -0600

    Add build.rs and php_zts/php_debug

--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,12 @@ mod zend;
 #[no_mangle]
 pub extern "C" fn get_module() -> *mut zend::ModuleEntry {
     Box::leak(Box::new(zend::ModuleEntry {
-        zend_api: 20230831,
-        name: b"native\0".as_ptr().cast(),
-        build_id: b"API20230831,NTS\0".as_ptr().cast(),
-        ..Default::default()
-    }))
-}
-
-/*
-#[no_mangle]
-pub extern "C" fn get_module() -> *mut zend::ModuleEntry {
-    Box::leak(Box::new(zend::ModuleEntry {
         size: core::mem::size_of::<zend::ModuleEntry>() as libc::c_ushort,
         zend_api: 20230831,
-        zend_debug: 0,
         name: b"native\0".as_ptr().cast(),
         build_id: b"API20230831,NTS\0".as_ptr().cast(),
+        zend_debug: cfg!(php_debug) as u8,
+        zts: cfg!(php_zts) as u8,
         ..Default::default()
     }))
 }
-*/
